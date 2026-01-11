9683094f5d2f10687e34441bc4cc64098be7f1a5
JeanCarlosAssuncao <jonathan.siqueira.129076@a.fecaf.com.br>
2026-01-11 19:27:35 +0000

    feat: classe gerenciador de tarefas, adicionar tarefa

---
+++ src/models/GerenciadorTarefas.py
@@ -0,0 +1,14 @@
+from BancoDeDados import BancoDeDados
+
+class GerenciadorTarefas:
+    def __init__(self):
+        self._tarefas = []
+    
+    def adicionar_tarefa(self, tarefa: object) -> tuple:
+        try:
+            banco = BancoDeDados()
+            resultado = banco.inserir_tarefa(tarefa.titulo, tarefa.descricao, tarefa.prazo, tarefa.status, tarefa.prioridade)
+            self._tarefas = banco.listar_tarefas()
+            return resultado
+        except Exception as error:
+            return False, error
